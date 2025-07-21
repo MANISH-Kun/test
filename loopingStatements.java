8ed035c5f153d6b9ece0fb8875354b8258a036b5
Manish-Kun <manisheng2004git@gmail.com>
2025-07-21 15:34:07 +0530

    bihari coding!

---
+++ loopingStatements.java
@@ -0,0 +1,23 @@
+public class loopingStatements {
+    public static void main(String[] args) {
+        // while loop
+        int i=0;
+        while(i<=2){
+            System.out.println("while loop executed:"+i+"time");
+            i++;
+        }
+        // do while loop
+        do {
+            System.out.println("do-while loop executed"+i+"time");
+            i++;
+            
+        } while (i<=4);
+
+        // for loop 
+
+        for(i=0;i<=2;i++){
+            System.out.println("for loop executed :"+i+"th time");
+        }
+
+    }
+}
