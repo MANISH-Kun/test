4f6215cecc93f28bcc27ecff5eaeb69f3cddbb78
Manish-Kun <manisheng2004git@gmail.com>
2025-07-22 09:52:42 +0530

    factorial

---
+++ factorial.java
@@ -0,0 +1,12 @@
+public class factorial {
+    public static void main(String[] args) {
+        int number=5;
+        int factorial=1;
+        for(int i=number;i>0;i--){
+            factorial=factorial*i;
+
+
+        }
+        System.out.println(factorial);
+    }
+}
