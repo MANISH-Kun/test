52ddee0a563763ee6d326bc638be220ac0948126
Manish-Kun <manisheng2004git@gmail.com>
2025-07-22 09:47:50 +0530

    perfectnumber

--- perfectnumber.java
+++ perfectnumber.java
@@ -1,15 +1,15 @@
 public class perfectnumber {
     public static void main(String[] args) {
-        int number = 6;
+        int number = 255;
         int factors=0;
-        for(int i=1;i<=number-1;i++){
+        for(int i=1;i<=number-1;i++){  // for optimized code for(int i=1;i<=number/2;i++)
             if(number%i==0){
                 factors=factors+i;
 
             }
         }
         if(factors==number){
-            System.out.println("the number is perfect number");
+            System.out.println(number+"the number is perfect number");
     }
     else{
         System.out.println("not a perfect no");
