c5c220eeff4cc6e901530696a93d7a6d37dc9b6b
Manish-Kun <manisheng2004git@gmail.com>
2025-08-05 10:32:31 +0530

    Person

---
+++ Person.java
@@ -0,0 +1,19 @@
+public class Person{
+    String name;
+    int age;
+    char gender;
+    long adharCard;
+    String panCard;
+    long licenceNumber;
+
+    public Person(String name,int age,char gender,long adharCard ,String panCard,long licenceNumber) {
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+        this.adharCard=adharCard;
+        this.panCard=panCard;
+        this.licenceNumber=licenceNumber;
+    }
+
+    
+}
